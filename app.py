ff91a79e7b5feb61a10ff6c6dfae8954d709cb74
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-07 22:50:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,34 +7,10 @@ app = Flask(__name__)
 def success(passWord):
    return render_template ('success.html')
 
-@app.route('/securityCheckerRedirect', methods = ['POST', 'GET'])
-   if request.method == 'POST':
-      security = request.form['security']
-      return redirect (url_for('securityChecker'))
-   else
-      security = request.args.get('security')
-      return redirect (url_for('securityChecker'))
-   
-@app.route('/breachCheckerRedirect', methods = ['POST', 'GET'])
-   if request.method == 'POST':
-      breach = request.form['breach']
-      return redirect (url_for('breachChecker'))
-   else
-      security = request.args.get('breach')
-      return redirect (url_for('breachChecker'))
-      
 @app.route('/creatorHome')
 def creatorHome():
    return render_template ('creator.html')
 
-@app.route('/securityChecker')
-def creatorHome():
-   return render_template ('securityChecker.html')
-
-@app.route('/breachChecker')
-def creatorHome():
-   return render_template ('breachChecker.html')
-    
 @app.route('/checker', methods = ['POST', 'GET'])
 def checker():
     if request.method == 'POST':
@@ -56,4 +32,4 @@ def index():
    
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
+app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
