1555ef07332e9b9f2c27c98d52f8939615aee8fb
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-07 22:22:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,48 @@
 # app.py
 import os
+from flask import Flask, request, jsonify, render_template, url_for, redirect, flash
 from flask import Flask, request, jsonify, render_template, url_for, redirect, flash, session
 app = Flask(__name__)
 
-@app.route('/success')
-def success():
-   return "Password is: ".format(session.get('password'))
+@app.route('/success/<passWord>')
+def success(passWord):
+   message = ('The password you entered was ' + passWord)
+   return render_template ('success.html', message = message)
+   return render_template ('success.html')
 
 @app.route('/creatorHome')
 def creatorHome():
-   return '"Let us create you a more secure password!"'
-    
-@app.route('/checker', methods = ['POST', 'GET'])
-def checker():
-    if request.method == 'POST':
-        password = request.form['password']
-        session['password'] = password)
-        return redirect (url_for('success'))
+@@ -20,12 +19,22 @@ def checker():
     else:
         password = request.args.get('password')
-        session['password'] = password
-        return redirect (url_for('success'))
-    
+        return redirect (url_for('success',passWord = password))
+
+      if 'password' in session:
+         session['password'] = session.get('password')
+      else:
+         session['password'] = password
+
 @app.route('/creator', methods = ['POST', 'GET'])
 def creator():
     if request.method == 'POST':
         return redirect(url_for('creatorHome'))
-    
+
 @app.route('/deletePassword')
 def deletePassword():
    session.pop('password', None)
    return render_template('deleted.html')
-
+    
 # A welcome message to test our server
 @app.route('/')#, methods=['POST'])
 def index():
-   return render_template('index.html')
-
+    #if form.validate_on_submit():
+       # if 'bruteForcer' in request.form:
+       #     pass
+       #     
+       ## elif 'checker' in request.form:
+        #    pass
+            
+            return render_template('index.html')
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
