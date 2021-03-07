43ddd1644fb08acd4ce42297bb5f8bad0581e330
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-07 23:00:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,25 @@ app = Flask(__name__)
 
 @app.route('/success/<passWord>')
 def success(passWord):
-   flash('The password you entered was' + passWord)
-   return render_template ('success.html', message=message)
+   message = ('The password you entered was ' + passWord)
+   return render_template ('success.html', message = message)
 
-@app.route('/creatorHome')
+
+@app.route('/securityCheckerRedirect', methods = ['POST', 'GET'])
+   if request.method == 'POST':
+      return redirect (url_for('securityChecker'))
+
+@app.route('/securityChecker')
 def creatorHome():
-   return '"Let us create you a more secure password!"'
+   return render_template ('securityChecker.html')
+   
+@app.route('/breachCheckerRedirect', methods = ['POST', 'GET'])
+   if request.method == 'POST':
+      return redirect (url_for('breachChecker'))
+
+@app.route('/breachChecker')
+def creatorHome():
+   return render_template ('breachChecker.html')
     
 @app.route('/checker', methods = ['POST', 'GET'])
 def checker():
@@ -25,6 +38,10 @@ def checker():
 def creator():
     if request.method == 'POST':
         return redirect(url_for('creatorHome'))
+      
+@app.route('/creatorHome')
+def creatorHome():
+   return render_template ('creator.html')
     
 # A welcome message to test our server
 @app.route('/')#, methods=['POST'])
@@ -33,4 +50,4 @@ def index():
    
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
+    app.run(threaded=True, port = int(os.environ.get('PORT', 5000)))
