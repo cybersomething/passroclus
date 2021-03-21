1aea071468dbdcb4ed606f7e544d688516ec007c
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-21 15:47:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@
 import os
 import subprocess
 import sourcepy import Source
+import hashlib
+import requests
+import argparse
+import re
 from flask import Flask, request, jsonify, render_template, url_for, redirect, flash
 app = Flask(__name__)
 
@@ -27,13 +31,48 @@ def breachCheckerRedirect():
       return redirect (url_for('breachChecker'))
 
 @app.route('/breachChecker')
-def breachChecker():
-   return '''<!doctype html>
-   <label for="pat"Enter Your Password Below</label>
-  <p><input type="password" class="form-control form-control-lg" name="pat" placeholder="Enter your password here" required/></p>
-       </div>
-       <p><input type="submit" class="btn btn-primary btn-lg" value="Check Password" /></p>
-       </form>'''
+def breachChecker(passWord):
+   SHA1 = hashlib.sha1(passWord.encode('utf-8'))
+    hash_string = SHA1.hexdigest().upper()
+    prefix = hash_string[0:5]
+
+    header = {
+        'User-Agent': 'password checker'
+    }
+
+    url = "https://api.pwnedpasswords.com/range/{}".format(prefix)
+
+    req = requests.get(url, headers=header).content.decode('utf-8')
+    # split the result twice - each line into key, value pairs of hash-postfixes and the usage count.
+    hashes = dict(t.split(":") for t in req.split('\r\n'))
+
+    # add the prefix to the key values (hashes) of the hashes dictionary
+    hashes = dict((prefix + key, value) for (key, value) in hashes.items())
+
+    for item_hash in hashes:
+        if item_hash == hash_string:
+            print("\nOh no — pwned!")
+            print("{} has previously appeared in a data breach, used {} times, and should never be used. ".format(passWord,hashes[hash_string]))
+            break
+
+    if hash_string != item_hash:
+        print("\nGood news — no pwnage found!")
+        print("{} wasn't found in any of the Pwned Passwords loaded into Have I Been Pwned.".format(passWord))
+
+    exit()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--password", help="enter your password")
+args = parser.parse_args()
+
+argv = vars(args)
+passWord = argv['password']
+
+if args.password:
+    check_leak(passWord)
+else:
+    print("No password supplied\n")
+    parser.print_help()
    return render_template ('breachChecker.html')
     
 @app.route('/checker', methods = ['POST', 'GET'])
