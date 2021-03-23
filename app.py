a5441401c4fb29f5e2fe8dc92474488af9e703e3
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-23 16:19:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -35,52 +35,52 @@ def breachCheckerRedirect(passWord):
 @app.route('/breachChecker/<passWord>')
 def breachChecker(passWord):
    def contains(required_chars, s):
-    return any(c in required_chars for c in s)
+      return any(c in required_chars for c in s)
 
    def contains_upper(s):
-    return contains(ascii_uppercase, s)
+      return contains(ascii_uppercase, s)
 
    def contains_lower(s):
-    return contains(ascii_lowercase, s)
+      return contains(ascii_lowercase, s)
 
    def contains_digit(s):
-    return contains(digits, s)
+      return contains(digits, s)
 
    def contains_special(s):
-    return contains(r"""!@$%^&*()_-+={}[]|\,.></?~`"':;""", s)
+      return contains(r"""!@$%^&*()_-+={}[]|\,.></?~`"':;""", s)
 
    def long_enough(s):
-    return len(s) >= 8
+      return len(s) >= 8
 
    def check_len(input):
-    if len(input) >= 8:
-        return True
-    else:
-        return False
+      if len(input) >= 8:
+         return True
+      else:
+         return False
 
    def validate_password(password):
-    VALIDATIONS = (
+      VALIDATIONS = (
         (contains_upper, 'Password needs at least one upper-case character.'),
         (contains_lower, 'Password needs at least one lower-case character.'),
         (contains_digit, 'Password needs at least one number.'),
         (contains_special, 'Password needs at least one special character.'),
         (long_enough, 'Password needs to be at least 8 characters in length.'),
-    )
-    failures = [
+      )
+      failures = [
         failMsg for validator, failMsg in VALIDATIONS if not validator(password)
-    ]
-    if not failures:
+      ]
+      if not failures:
         return True
-    else:
-        print("Invalid password! Review below and change your password accordingly!\n")
-        for failMsg in failures:
+      else:
+         print("Invalid password! Review below and change your password accordingly!\n")
+         for failMsg in failures:
             msg = "Your password is insecure \n"
             strengthCheck = msg + failMsg
             return msg + failMsg
             break
-
-    if __name__ == '__main__':
-        while True:
+           
+      if __name__ == '__main__':
+         while True:
             password = validate_password(password)
             if validate_password(password):
                 strengthCheck = ("Password meets all requirements and may be used.\n")
