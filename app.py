4a6df3d0aab2ce813d318614678e89b2d1375ec4
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-23 16:16:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -37,28 +37,28 @@ def breachChecker(passWord):
    def contains(required_chars, s):
     return any(c in required_chars for c in s)
 
-def contains_upper(s):
+   def contains_upper(s):
     return contains(ascii_uppercase, s)
 
-def contains_lower(s):
+   def contains_lower(s):
     return contains(ascii_lowercase, s)
 
-def contains_digit(s):
+   def contains_digit(s):
     return contains(digits, s)
 
-def contains_special(s):
+   def contains_special(s):
     return contains(r"""!@$%^&*()_-+={}[]|\,.></?~`"':;""", s)
 
-def long_enough(s):
+   def long_enough(s):
     return len(s) >= 8
 
-def check_len(input):
+   def check_len(input):
     if len(input) >= 8:
         return True
     else:
         return False
 
-def validate_password(password):
+   def validate_password(password):
     VALIDATIONS = (
         (contains_upper, 'Password needs at least one upper-case character.'),
         (contains_lower, 'Password needs at least one lower-case character.'),
