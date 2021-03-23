ef6101cae3c60432f4f9f2c6e1cffd103b905eb3
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-23 17:15:46 +0000

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,59 @@ def breachCheckerRedirect(passWord):
    if request.method == 'POST':
       return redirect (url_for('breachChecker/<passWord>'))
 
+#@app.route('/breachChecker/<passWord>')
+#def breachChecker(passWord):
+#   strengthCheck = passwordCheck.validate_password(passWord)
+#   return render_template('breachChecker.html', strengthCheck = strengthCheck)
+
 @app.route('/breachChecker/<passWord>')
 def breachChecker(passWord):
-   strengthCheck = passwordCheck.validate_password(passWord)
-   return render_template('breachChecker.html', strengthCheck = strengthCheck)
+   def contains(required_chars, s):
+         return any(c in required_chars for c in s)
+
+   def contains_upper(s):
+       return contains(ascii_uppercase, s)
+
+   def contains_lower(s):
+       return contains(ascii_lowercase, s)
+
+   def contains_digit(s):
+       return contains(digits, s)
+
+   def contains_special(s):
+       return contains(r"""!@$%^&*()_-+={}[]|\,.></?~`"':;""", s)
+
+   def long_enough(s):
+       return len(s) >= 8
+
+   def check_len(input):
+       if len(input) >= 8:
+           return True
+       else:
+           return False
+
+   def validate_password(password):
+       VALIDATIONS = (
+           (contains_upper, 'Password needs at least one upper-case character.'),
+           (contains_lower, 'Password needs at least one lower-case character.'),
+           (contains_digit, 'Password needs at least one number.'),
+           (contains_special, 'Password needs at least one special character.'),
+           (long_enough, 'Password needs to be at least 8 characters in length.'),
+       )
+       failures = [
+           msg for validator, msg in VALIDATIONS if not validator(password)]
+       if not failures:
+           return True
+       else:
+         return render_template('breachChecker.html', strengthCheck = failures)
+
+       if __name__ == '__main__':
+           while True:
+               password = validate_password(password)
+               if validate_password(password):
+                   msg = ("Password meets all requirements and may be used.\n")
+                   return render_template('breachChecker.html', strengthCheck = msg)
+                   break
     
 @app.route('/checker', methods = ['POST', 'GET'])
 def checker():
