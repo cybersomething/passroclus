5ac4b3f27c6527614de81d1954ce2e4386396d10
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-24 18:27:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,19 +17,62 @@ def securityCheckerRedirect():
    if request.method == 'POST':
       return redirect (url_for('securityChecker'))
 
-@app.route('/securityChecker')
-def securityChecker():
-   return render_template ('securityChecker.html')
+@app.route('/securityChecker/<passWord>')
+def securityChecker(password):
+   def contains(required_chars, s):
+      return any(c in required_chars for c in s)
+
+def contains_upper(s):
+    return contains(ascii_uppercase, s)
+
+def contains_lower(s):
+    return contains(ascii_lowercase, s)
+
+def contains_digit(s):
+    return contains(digits, s)
+
+def contains_special(s):
+    return contains(r"""!@$%^&*()_-+={}[]|\,.></?~`"':;""", s)
+
+def long_enough(s):
+    return len(s) >= 8
+
+def check_len(input):
+    if len(input) >= 8:
+        return True
+    else:
+        return False
+
+def validate_password(passWord):
+    VALIDATIONS = (
+        (contains_upper, 'Password needs at least one upper-case character.'),
+        (contains_lower, 'Password needs at least one lower-case character.'),
+        (contains_digit, 'Password needs at least one number.'),
+        (contains_special, 'Password needs at least one special character.'),
+        (long_enough, 'Password needs to be at least 8 characters in length.'),
+    )
+    failures = [
+        msg for validator, msg in VALIDATIONS if not validator(password)]
+    if not failures:
+        return True
+    else:
+        firstMessage = ("Invalid password! Review below and change your password accordingly!\n")
+        for msg in failures:
+            return msg
+           
+    if __name__ == '__main__':
+        while True:
+            password = validate_password(password)
+            if validate_password(password):
+                firstMessage = ("Password meets all requirements and may be used.\n")
+                return firstMessage
+                break
+   return render_template ('securityChecker.html', firstMessage = firstMessage, msg = msg)
    
 @app.route('/breachCheckerRedirect/<passWord>', methods = ['POST', 'GET'])
 def breachCheckerRedirect(passWord):
    if request.method == 'POST':
       return redirect (url_for('breachChecker/<passWord>'))
-
-#@app.route('/breachChecker/<passWord>')
-#def breachChecker(passWord):
-#   strengthCheck = passwordCheck.validate_password(passWord)
-#   return render_template('breachChecker.html', strengthCheck = strengthCheck)
 
 @app.route('/breachChecker/<passWord>')
 def breachChecker(passWord):
@@ -45,10 +88,10 @@ def breachChecker(passWord):
 def checker():
     if request.method == 'POST':
         password = request.form['password']
-        return redirect (url_for('breachChecker',passWord = password))
+        return redirect (url_for('securityChecker',passWord = password))
     else:
         password = request.args.get('password')
-        return redirect (url_for('breachChecker',passWord = password))
+        return redirect (url_for('securityChecker',passWord = password))
     
 @app.route('/creator', methods = ['POST', 'GET'])
 def creator():
