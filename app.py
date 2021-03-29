9f4146efa3bd24268a2f1d474f52cc9d41cc7f32
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-29 13:30:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -106,53 +106,15 @@ def generatorRedirect():
 
 @app.route('/generator/<length>')
 def generator(length):  
-
-  length = int(length)
-  symbol = 0
-  lower = 0
-  upper = 0
-  number = 0
-  count = 0
-  password = []
-
-  
-#length = input("Please Enter Desired Password Length (default 16)\n")
-#length = 16 if length == '' else int(length)
-
-#randomly select ascii character classes and individual characters
-
-  while count < length:
-      rand = random.randint (0,3)
-      if rand == 0:
-          lower += 1
-          b = int(random.randint (97,123))
-          password.append(b)
-      elif rand == 1:
-          upper += 1
-          b = random.randint (65,91)
-          password.append(b)
-      elif rand == 2:
-          number += 1
-          b = random.randint (48,58)
-          password.append(b)
-      elif rand == 3:
-          r = random.randint(0,2)
-          symbol += 1
-          if r == 0:
-              b = random.randint (33,48)
-              password.append(b)
-          elif r == 1:
-              b = random.randint (91,97)
-              password.append(b)
-          elif r == 2:
-              b = random.randint (123,126)
-              password.append(b)
-      count += 1
-
-  #convert ascii code to characters
-  word = "".join([chr(c) for c in password])
-  contains = "This Password contains",lower,"lowercase,",upper,"uppercase,",number,"numbers and",symbol,"symbol characters"
-  return render_template ('generator.html', word = word, contains = contains)
+   length = int(length)
+   textFile = open('randomWords.txt', 'r')
+   passphrase = []
+   
+   for i in range(length):
+      temp = int(random.random()*len(textFile))
+      passphrase.append(textFile[temp])
+   
+  return render_template ('generator.html', passphrase = passphrase)
     
 # A welcome message to test our server
 @app.route('/')#, methods=['POST'])
