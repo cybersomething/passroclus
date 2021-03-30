a2cfce9a7106e18cc4fb1e574ead4fa6a45444ee
cybersomething <46792838+cybersomething@users.noreply.github.com>
2021-03-30 16:02:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -50,19 +50,14 @@ def securityChecker(passWord):
    
    if (entropy <= 27):
       rating = "Your passphrase is very weak, consider using our passphrase generator to create a new one"
-      return rating
-   if (entropy >= 28 && <= 35):
+   if entropy in range(28, 35):
       rating = "Your passphrase is weak, consider using our passphrase generator to create a new one"
-      return rating
    if (entropy >= 36 && <= 59):
       rating = "Your passphrase is reasonable, if you've not already - check out the breach checker to make sure your password is still secure"
-      return rating
    if (entropy >= 60 && <= 127):
       rating = "Your passphrase is strong! Well done, if you've not already check that your passphrase hasn't been breached and that it's still secure"
-      return rating
    if (entropy >= 128):
       rating = "Your passphrase is very strong, well done! Check that it's not been breached with our breach checker!"
-      return rating
    
    return render_template ('securityChecker.html', entropy = entropy, rating = rating)
 
